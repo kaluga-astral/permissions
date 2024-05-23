2398dcfb17619819d53268d7500e71c3e8e35067
Andrey Potyomkin <and_tem@icloud.com>
2024-05-23 13:44:10 +0300

    feat: Добавлены фабрики создания allowed и denial permission

--- package/src/index.ts
+++ package/src/index.ts
@@ -2,7 +2,13 @@ export { createPolicyManagerStore, PolicyManagerStore } from './stores';
 
 export { createRule } from './utils';
 
-export { Permission, DenialPermission, AllowedPermission } from './entities';
+export {
+  Permission,
+  DenialPermission,
+  AllowedPermission,
+  createAllowedPermission,
+  createDenialPermission,
+} from './entities';
 
 export type { DenialReason, Policy } from './types';
 
