2811fada94dbc0f46f3e2f8c09fe6b3f84ecaba5
Andrey Potyomkin <and_tem@icloud.com>
2024-05-23 11:39:39 +0300

    feat: Добавлен экспорт SystemDenialReason

--- package/src/index.ts
+++ package/src/index.ts
@@ -5,3 +5,5 @@ export { createRule } from './utils';
 export { Permission, DenialPermission, AllowedPermission } from './entities';
 
 export type { DenialReason, Policy } from './types';
+
+export { SystemDenialReason } from './enums';
